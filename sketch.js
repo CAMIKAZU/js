e3bdacf5d4de31e640dd95faca01611e259cfb6b
CAMIKAZU <00001093642531sp@al.educacao.sp.gov.br>
2024-06-11 15:54:13 -0300

    adicionando arquivo

--- sketch.js
+++ sketch.js
@@ -1,1 +1,16 @@
+let xbolinha = 300
+let ybolinha = 200
+let diametro= 22
 
+let velocidadexbolinha = 6;
+let velocidadeybolinha = 6;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background("");
+circle(xBolinha, yBolinha, diametro)
+  xBolinha += velocidadeXbolinha
+  yBolinha += velocidadeYbolinha
